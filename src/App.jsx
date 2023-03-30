5c2857cfc64a255b18d24802c6a99e53c71ef8f2
puturangga21 <puturangga21@gmail.com>
2023-03-31 00:50:37 +0800

    add route to search page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import NotFound from "./components/NotFound";
+import SearchPage from "./components/SearchPage";
 
 function App() {
    return (
       <Router>
          <Routes>
             <Route exact path="/" Component={HomePage} />
+            <Route path="/search-movie" Component={SearchPage} />
             <Route path="*" Component={NotFound} />
          </Routes>
       </Router>
